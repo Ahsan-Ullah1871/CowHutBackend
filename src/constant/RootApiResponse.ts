e774d5149aa222f66492b59b56ab23ad156090c2
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-03 02:10:39 +0600

    - root  api json updated

--- src/constant/RootApiResponse.ts
+++ src/constant/RootApiResponse.ts
@@ -1,59 +1,136 @@
 const root_api_response = {
   welcomeMessage:
     'Welcome to the Online Cow Selling Backend API documentation!',
-  mainUrl: 'https://assignment-3-cow-hut.vercel.app',
+  mainUrl: 'https://cow-hut-sand.vercel.app/',
   paths: [
     {
-      url: '/api/v1/auth/signup',
-      description: 'Register a new user',
-      methods: ['POST'],
+      group: 'Admin',
+      apis: [
+        {
+          url: '/api/v1/admins/create-admin',
+          description: 'Register a new admin',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/admins/login',
+          description: 'Admin login',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/admins/refresh-token',
+          description: 'Refresh admin access token',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/admins/my-profile',
+          description: 'Admin profile',
+          methods: ['GET'],
+        },
+        {
+          url: '/api/v1/admins/my-profile',
+          description: 'Admin profile update',
+          methods: ['PATCH'],
+        },
+      ],
+    },
+    {
+      group: 'Auth (User)',
+      apis: [
+        {
+          url: '/api/v1/auth/signup',
+          description: 'Register a new user',
+          methods: ['POST'],
+        },
+
+        {
+          url: '/api/v1/auth/login',
+          description: 'User login',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/auth/refresh-token',
+          description: 'Refresh user access token',
+          methods: ['POST'],
+        },
+      ],
     },
 
     {
-      url: '/api/v1/users',
-      description: 'Retrieve a list of all users',
-      methods: ['GET'],
+      group: 'Users',
+      apis: [
+        {
+          url: '/api/v1/users',
+          description: 'Retrieve a list of all users',
+          methods: ['GET'],
+        },
+        {
+          url: '/api/v1/users/{user_id}',
+          description:
+            'Retrieve details of a specific user / update / delete auser',
+          methods: ['GET', 'PATCH', 'DELETE'],
+        },
+        {
+          url: '/api/v1/users/my-profile',
+          description: 'User profile',
+          methods: ['GET'],
+        },
+        {
+          url: '/api/v1/users/my-profile',
+          description: 'User profile update',
+          methods: ['PATCH'],
+        },
+      ],
     },
     {
-      url: '/api/v1/users/{user_id}',
-      description: 'Retrieve details of a specific user',
-      methods: ['GET', 'PATCH', 'DELETE'],
+      group: 'Products',
+      apis: [
+        {
+          url: '/api/v1/cows',
+          description: 'Retrieve a list of all cows',
+          methods: ['GET'],
+        },
+        {
+          url: '/api/v1/cows/{cow_id}',
+          description: 'Retrieve details of a specific cow',
+          methods: ['GET'],
+        },
+        {
+          url: '/api/v1/cows',
+          description: 'Create a new cow',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/cows/{cow_id}',
+          description: 'Update details of a specific cow',
+          methods: ['PATCH'],
+        },
+        {
+          url: '/api/v1/cows/{cow_id}',
+          description: 'Delete a specific cow',
+          methods: ['DELETE'],
+        },
+      ],
     },
     {
-      url: '/api/v1/cows',
-      description: 'Retrieve a list of all cows',
-      methods: ['GET'],
-    },
-    {
-      url: '/api/v1/cows/{cow_id}',
-      description: 'Retrieve details of a specific cow',
-      methods: ['GET'],
-    },
-    {
-      url: '/api/v1/cows',
-      description: 'Create a new cow',
-      methods: ['POST'],
-    },
-    {
-      url: '/api/v1/cows/{cow_id}',
-      description: 'Update details of a specific cow',
-      methods: ['PATCH'],
-    },
-    {
-      url: '/api/v1/cows/{cow_id}',
-      description: 'Delete a specific cow',
-      methods: ['DELETE'],
-    },
-    {
-      url: '/api/v1/orders',
-      description: 'Retrieve a list of all orders',
-      methods: ['GET'],
-    },
+      group: 'Orders',
+      apis: [
+        {
+          url: '/api/v1/orders',
+          description: 'Retrieve a list of all orders',
+          methods: ['GET'],
+        },
 
-    {
-      url: '/api/v1/orders',
-      description: 'Create a new order',
-      methods: ['POST'],
+        {
+          url: '/api/v1/orders',
+          description: 'Create a new order',
+          methods: ['POST'],
+        },
+        {
+          url: '/api/v1/orders/{order_id}',
+          description: 'Order Details',
+          methods: ['GET'],
+        },
+      ],
     },
   ],
 }
